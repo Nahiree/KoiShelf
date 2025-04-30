702f3631848822cf44977145e864d5170d6f4649
Nahiree <nahir.casablanca@ucb.edu.bo>
2025-04-30 17:15:01 -0400

    Cambios en el componente menu (declaración en standalone y html) y aplicación del componente en login

--- koishelfApp/src/app/components/menu/menu.component.ts
+++ koishelfApp/src/app/components/menu/menu.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  standalone:false,
 })
 export class MenuComponent  implements OnInit {
 
--- koishelfApp/src/app/pages/login/login.module.ts
+++ koishelfApp/src/app/pages/login/login.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -17,6 +17,7 @@ import { SharedModule } from 'src/app/modules/shared/shared.module';
     SharedModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginPageModule {}
