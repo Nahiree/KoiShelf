7866a79a43365576a691901f04e834dc73c0129a
Nahiree <nahir.casablanca@ucb.edu.bo>
2025-05-16 11:05:21 -0400

    complementación del login, aplicación de tipografías y corrección de perfil.

--- koishelfApp/src/app/pages/profile/profile.page.ts
+++ koishelfApp/src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+  ) { }
 
   ngOnInit() {
   }
 
 }
+
