b396e0ea1d9a667d943bd616caa9b71e0705997f
Galilea Cossio <ana.cossio@ucb.edu.bo>
2025-05-24 23:33:18 -0400

    Mangateca maqueta, pero no funcional

--- koishelfApp/src/app/pages/mangateca/mangateca.page.ts
+++ koishelfApp/src/app/pages/mangateca/mangateca.page.ts
@@ -8,6 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MangatecaPage implements OnInit {
 
+  mangas = [
+    {
+      titulo: 'Haikyuu!!',
+      imagen: 'assets/manga-cover/fma.jpg',
+      progreso: 1,
+      estado: 'Completada!'
+    },
+    {
+      titulo: 'Yu Yu Hakusho',
+      imagen: 'assets/manga-cover/fma.jpg',
+      progreso: 0.4,
+      estado: 'Te faltan 6 tomos'
+    },
+    {
+      titulo: 'Boy meets Maria',
+      imagen: 'assets/manga-cover/fma.jpg',
+      progreso: 1,
+      estado: 'Tomo Único'
+    },
+    {
+      titulo: 'Neon Genesis Evangelion',
+      imagen: 'assets/manga-cover/fma.jpg',
+      progreso: 0.6,
+      estado: 'Te faltan 3 tomos'
+    },
+    {
+      titulo: 'Oyasumin Punpun',
+      imagen: 'assets/manga-cover/fma.jpg',
+      progreso: 0.1,
+      estado: 'Te faltan varios tomos'
+    },
+    {
+      titulo: 'Oyasumin Punpun',
+      imagen: 'assets/manga-cover/fma.jpg',
+      progreso: 0.1,
+      estado: 'Te faltan varios tomos'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
