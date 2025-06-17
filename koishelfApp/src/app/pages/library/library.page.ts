171bcef8dbba24adb6f9b345a79fbc83b1ed106d
Samara <scamargol16o4@gmail.com>
2025-06-17 06:40:18 -0400

    botón corazón funcional

--- koishelfApp/src/app/pages/library/library.page.ts
+++ koishelfApp/src/app/pages/library/library.page.ts
@@ -1,37 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from 'src/app/services/database.service';
+import { Router } from '@angular/router';
 
-@Component({
+
+@Component({ 
   selector: 'app-library',
   templateUrl: './library.page.html',
   styleUrls: ['./library.page.scss'],
-  standalone:false
+  standalone: false
 })
 export class LibraryPage implements OnInit {
-  favoritosIds: string[] = [];        // Aquí se guarda el array de ids de favoritos
-  mangasFavoritos: any[] = [];        // Aquí se guarda el resultado final con los datos completos de cada manga
-segmentValue: string = 'terminados';
-  constructor(private db: DatabaseService) {}
+  favoritosIds: string[] = []; // IDs de favoritos
+  heartIds: string[] = [];     // IDs de "heart"
+
+  mangasFavoritos: any[] = []; // Detalles de mangas en favoritos
+  mangasHeart: any[] = [];    // Detalles de mangas en "heart"
+
+  segmentValue: string = 'favoritos';
+
+  constructor(private db: DatabaseService, private router: Router) { }
+
+  goToMangaDetail(mangaId: string) {
+  this.router.navigate(['/manga-detail', mangaId]);
+}
+
 
   ngOnInit(): void {
-    // Paso 1: obtener el UID desde el localStorage
-    
     const profile = localStorage.getItem('profile');
     const userId = profile ? JSON.parse(profile).id : '';
-
-    // Paso 2: si hay UID, obtenemos el documento del usuario
     if (userId) {
       this.db.getDocumentById('users', userId).subscribe(userDoc => {
-        this.favoritosIds = userDoc?.library || [];
-        console.log(userDoc?.library);
-        console.log(this.favoritosIds);
-        // Paso 3: ahora buscamos los datos completos de esos mangas
+        this.favoritosIds = userDoc?.library || []; // IDs de Favoritos
+        this.heartIds = userDoc?.heartLibrary || []; // IDs de Heart
+
         this.db.fetchFirestoreCollection('manga').subscribe(mangas => {
-          // Paso 4: filtramos solo los mangas cuyos ID están en favoritos
-          this.mangasFavoritos = mangas.filter(m => this.favoritosIds.includes(m.id));
-          console.log(this.mangasFavoritos)
+          this.mangasFavoritos = mangas.filter(m => this.favoritosIds.includes(m.id)); // Filtra Favoritos
+          this.mangasHeart = mangas.filter(m => this.heartIds.includes(m.id)); // Filtra Heart
         });
+
       });
     }
   }
 }
+
